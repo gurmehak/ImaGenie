bbe17054c7725a0762af709eaf89dc753d5a3247
yfan810 <fanyuhan1234@gmail.com>
2025-01-13 22:24:39 -0800

    complete scale body function and test image

--- src/imagenie/scale.py
+++ src/imagenie/scale.py
@@ -1,29 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.transform import rescale
+
 
 def scale(image, N):
     """Scale an image file by a factor of N.
 
     Parameters
     ----------
-    image : np array
-        An image file to be scaled.
+    image : np.array
+        The input image to be flipped, represented as a NumPy array or similar format.
     
-    N: integer
-        An integer specifying the scaling factor.
+    N: float
+        An non-negative float specifying the scaling factor.
 
     Returns
     -------
-    np array
-        An image file that has been scaled.
-        
-    Raises:
-    ------
-    ValueError
-        If the factor N is not an integer.
+    np.array
+        The flipped image as a NumPy array.
 
     Examples
     --------
     Scale the image 2 times larger. 
     >>> img = scale("testimage.jpg", 2)
     """
+    scaled_array = rescale(img, scale=N, channel_axis=-1, anti_aliasing=True)
     
-    pass
+    return scaled_array
