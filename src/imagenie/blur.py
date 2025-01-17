d00732d0b1cec98dbd8eb74d28b04b70a923a01e
Agam Sanghera <agamsanghera911@gmail.com>
2025-01-17 12:06:07 -0800

    basic errors and warnings created

--- src/imagenie/blur.py
+++ src/imagenie/blur.py
@@ -1,3 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import warnings
 def blur(image, stdev=1.0,radius=None):
     """
     Add noise to image using a Gaussian filter
@@ -44,3 +48,33 @@ def blur(image, stdev=1.0,radius=None):
         [0.4977027 , 0.5357282 , 0.5736908 ]
     """
     pass
+
+    if stdev is not None:
+        if not isinstance(stdev, (int, float)):
+            raise TypeError("Standard deviation must be a numeric value (int or float).")
+        if stdev <= 0:
+            raise ValueError("Standard deviation must be a positive number.")
+    # Calculate new size. If N is None, return original image numpy array
+    else:
+        return image
+    
+    if radius is not None:
+        if not isinstance(radius, (int, float)):
+            raise TypeError("Radius of Gaussian kernel must be a numeric value (int or float).")
+        if radius <= 0:
+            raise ValueError("Radius of Gaussian kernel must be a positive number.")
+    
+    if not isinstance(image, np.ndarray):
+        raise TypeError("Image must be a numpy array")
+    
+    #Warn if the original image exceeds 1028 * 1028
+    if image.shape[0] > 1028 or image.shape[1] > 1028:
+        warnings.warn("The input image exceeds the maximum size of 1028x1028, resizing to 1028x1028 and working with it")
+        if image.shape[0] > 1028 or image.shape[1] > 1028:
+            image=cv2.resize(image, (0, 0), fx = 1028, fy = 1028)
+        elif image.shape[0]>1028:
+            image=cv2.resize(image, (0, 0), fx = 1028, fy = image.shape[1])
+        elif image.shape[1]>1028:
+            image=cv2.resize(image, (0, 0), fy = 1028, fx = image.shape[0])
+
+
