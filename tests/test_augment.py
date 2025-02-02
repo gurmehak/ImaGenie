74d14404876b2bd85efbeb19e6d1dd8809fd386f
gurmehak <gurmehak99@gmail.com>
2025-02-02 00:34:27 -0800

    add two test cases for checking correctness - ensure all conditions have 2 test cases

--- tests/test_augment.py
+++ tests/test_augment.py
@@ -22,6 +22,7 @@ def test_augment():
     output_image = np.asarray(plt.imread('tests/output.png'))
     output_image = output_image[:,:,0:3]
     invalid_input = ["not_an_array"]
+    input_single = np.asarray(plt.imread("tests/testimage.png"))
 
     # Operations
     operations = [(flip, 1), (scale, 0.5), (blur, 1)]
@@ -39,9 +40,13 @@ def test_augment():
 
     assert isinstance(input_image, list), "Input must be a list of images."
     
+
     # Test 2: Check if operations are applied correctly
     assert np.isclose(blur(input_image[0]),output_image,atol=.01).all, "Incorrect output, function not working as expected"
+
+    assert np.isclose(augment(input_image, operations), blur(scale(flip(input_single,1),0.5),1),atol=.01).all, "Incorrect output, function not working as expected"
     
+
     # Test 3: Check if invalid functions raise a ValueError
     invalid_operations = [(blur,1),(flip, 1), (scale, 1), (lambda x: x, 1)]  # Invalid function (lambda)
     with pytest.raises(ValueError):
@@ -50,6 +55,7 @@ def test_augment():
     invalid_operations_2 = [(flip, 1), (scale, 0.5), (blur, 1), (lambda x: x, 1)]
     with pytest.raises(ValueError):
         augment(input_image, invalid_operations_2)
+
 
     # Test 4: Check if the result is a list of NumPy arrays
     assert isinstance(augmented_images, list), "Output should be a list of images."
